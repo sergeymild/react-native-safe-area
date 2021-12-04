198323818a13f60677cdd1b2c90c8f2b62f41915
Sergei Golishnikov <sergeymild@yandex.ru>
2021-12-04 08:53:10 +0200

    android safe area

--- android/src/main/java/com/reactnativesafearea/SafeAreaModule.java
+++ android/src/main/java/com/reactnativesafearea/SafeAreaModule.java
@@ -1,34 +1,51 @@
 package com.reactnativesafearea;
 
+import android.view.View;
+import android.view.WindowInsets;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.uimanager.PixelUtil;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @ReactModule(name = SafeAreaModule.NAME)
 public class SafeAreaModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "SafeArea";
+  public static final String NAME = "SafeArea";
 
-    public SafeAreaModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-    }
+  public SafeAreaModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
-    }
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
 
-
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
-    }
-
-    public static native int nativeMultiply(int a, int b);
+  @Nullable
+  @Override
+  public Map<String, Object> getConstants() {
+    View rootView = getCurrentActivity().getWindow().getDecorView().findViewById(android.R.id.content);
+    EdgeInsets insets = SafeAreaUtils.getSafeAreaInsets(rootView);
+    HashMap<String, Object> constants = new HashMap<>(3);
+    if (insets == null) return constants;
+    constants.put("insetTop", PixelUtil.toDIPFromPixel(insets.top));
+    constants.put("insetBottom", PixelUtil.toDIPFromPixel(insets.bottom));
+    constants.put("statusBarHeight", PixelUtil.toDIPFromPixel(
+      getCurrentActivity()
+        .getResources()
+        .getIdentifier("status_bar_height", "dimen", "android")
+    ));
+    return constants;
+  }
 }
