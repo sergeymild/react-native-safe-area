7397022699e42a8b1e6d3ae0ecc99722128665ed
sergeymild <sergeymild@yandex.ru>
2024-01-01 17:13:00 +0200

    web

---
+++ src/safeArea.web.ts
@@ -0,0 +1,13 @@
+export const safeArea = {
+  get statusBarHeight(): number {
+    return 0;
+  },
+
+  get topSafeArea(): number {
+    return 0;
+  },
+
+  get bottomSafeArea(): number {
+    return 0;
+  },
+};
